bf650c9142d3bcce2ba7d08db1d0a3f06b5b2677
William Fedder <fedderw@gmail.com>
2022-04-30 05:33:44 -0400

    run black on charts.py

--- charts.py
+++ charts.py
@@ -1,39 +1,61 @@
 import plotly.express as px
 
+
 def create_bar_chart(dataframe):
-  df = dataframe
+    df = dataframe
 
-  fig = px.bar(df, x="age_group", y="total_pov_change",
-              labels={
-                     "total_pov_change_%": "<b>Change in child poverty rate<b>",
-                     "total_pov_rate_%": "<b>Original child poverty rate<b>",
-                     "new_total_pov_rate_%": "<b>New child poverty rate<b>",
-                    'age_group':'<b>Age Group<b>', 'total_pov_change':'<b>Change in the SPM poverty rate<b>'
-                 }
-               ,
-               hover_data={'total_pov_rate_%':True,'new_total_pov_rate_%':True,'total_pov_change_%':True,
-                           'age_group':False,'total_pov_change':False}
-               ,
-             #   title="<b>Change in child poverty by age group<b>",
-             color='age_group',
-             text='total_pov_change',
-             color_discrete_map={
-                                 'All': '#003f5c',
-                                 'Adult': '#5886a5',
-                                 'Child': '#5886a5',
-                                 'Young Child': '#5886a5'})
+    fig = px.bar(
+        df,
+        x="age_group",
+        y="total_pov_change",
+        labels={
+            "total_pov_change_%": "<b>Change in child poverty rate<b>",
+            "total_pov_rate_%": "<b>Original child poverty rate<b>",
+            "new_total_pov_rate_%": "<b>New child poverty rate<b>",
+            "age_group": "<b>Age Group<b>",
+            "total_pov_change": "<b>Change in the SPM poverty rate<b>",
+        },
+        hover_data={
+            "total_pov_rate_%": True,
+            "new_total_pov_rate_%": True,
+            "total_pov_change_%": True,
+            "age_group": False,
+            "total_pov_change": False,
+        },
+        #   title="<b>Change in child poverty by age group<b>",
+        color="age_group",
+        text="total_pov_change",
+        color_discrete_map={
+            "All": "#003f5c",
+            "Adult": "#5886a5",
+            "Child": "#5886a5",
+            "Young Child": "#5886a5",
+        },
+    )
 
-  fig.update_layout(showlegend=False, yaxis_ticksuffix='%',plot_bgcolor='#f7f7f7',
-                    paper_bgcolor='#f7f7f7' ,margin=dict(l=0, r=0, t=0, b=0),
-                    title_x=0.5,font=dict(color='black',size=12))
-  fig.update_traces(texttemplate='%{text}%')
-  fig.update_xaxes(showgrid=False, showline=True, zeroline=False,linecolor='black')
-  fig.update_yaxes(showgrid=False, showline=True, zeroline=False,linecolor='black')
-  return fig
-#D1D3D4 grey
-#f7f7f7 light grey
-#fffbff off white
-'''
+    fig.update_layout(
+        showlegend=False,
+        yaxis_ticksuffix="%",
+        plot_bgcolor="#f7f7f7",
+        paper_bgcolor="#f7f7f7",
+        margin=dict(l=0, r=0, t=0, b=0),
+        title_x=0.5,
+        font=dict(color="black", size=12),
+    )
+    fig.update_traces(texttemplate="%{text}%")
+    fig.update_xaxes(
+        showgrid=False, showline=True, zeroline=False, linecolor="black"
+    )
+    fig.update_yaxes(
+        showgrid=False, showline=True, zeroline=False, linecolor="black"
+    )
+    return fig
+
+
+# D1D3D4 grey
+# f7f7f7 light grey
+# fffbff off white
+"""
 
 
 
@@ -63,4 +85,4 @@ logo_img=html.Div( html.Img(src='data:image/jpg;base64,{}'.format(encoded.decode
                   )
                    ,style=dict(paddingTop='',paddingBottom='',display='inline-block'))
 
-    '''
+    """
